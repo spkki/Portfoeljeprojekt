72d497f83e5b7dc67e7cfca6aebda681741f79ed
spkki <Svend.b.t@hotmail.dk>
2024-04-23 12:18:51 +0200

    Added database support
    Next steps: Contiue work on enemy class and start working on a function
    to save and load charactar

---
+++ datamanager.h
@@ -0,0 +1,29 @@
+//
+// Created by Svend on 23-04-2024.
+//
+
+#pragma once
+
+#include <QCoreApplication>
+#include <QDebug>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+
+bool openDatabase(QSqlDatabase& database){ //Open database function
+    database = QSqlDatabase::addDatabase("QSQLITE");
+    database.setDatabaseName("/home/spkki/workspace/software-udvikling/portfolio/Portfoeljeprojekt/database.db;");
+
+    if (!database.open()){
+        qWarning() << "Could not open database:" << database.lastError().text();
+        return false;
+    }
+
+    qDebug() << "Database succesfully opened";
+    return true;
+}
+
+void closeDatabase(QSqlDatabase& database){
+    database.close();
+    qDebug() << "Database succesfully closed";
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "enemy.h"
 #include "hero.h"
+#include "datamanager.h"
 #include <iostream>
+#include <QString>
 
 int main()
 {
@@ -15,6 +17,11 @@ int main()
     Test.gainXp(1000);
     Test.getXp();
     Test.getLevel();
+
+    QSqlDatabase database;
+    openDatabase(database);
+
+    closeDatabase(database);
 
     Enemy Orc("Orc1", 10, 1, 1);
     Orc.getXp();
